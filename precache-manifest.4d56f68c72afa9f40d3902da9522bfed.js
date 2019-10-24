1344516336eb946e42bd2897807ead84eb8c0bcb
Samuel <samuelnnj@gmail.com>
2019-10-24 17:34:31 +0100

    Updates

---
+++ precache-manifest.4d56f68c72afa9f40d3902da9522bfed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ebc3da20a584599a4c1d8f07f7cc50",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "48d9c63df5d1e8ddf03f",
+    "url": "/monsters-rolodex-app/static/css/main.9f0c79cb.chunk.css"
+  },
+  {
+    "revision": "7a5c6168d6112f6d77b9",
+    "url": "/monsters-rolodex-app/static/js/2.dbd7c3df.chunk.js"
+  },
+  {
+    "revision": "48d9c63df5d1e8ddf03f",
+    "url": "/monsters-rolodex-app/static/js/main.3a2a3e42.chunk.js"
+  },
+  {
+    "revision": "7d816ed5acab13f9f254",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.d9e1f870.js"
+  }
+]);
